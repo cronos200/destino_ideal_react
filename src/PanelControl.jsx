daf0276d4abaea77f2191afadca11f62ce786eed
juan david berrio builes <juanberriobuiles@gmail.com>
2025-05-03 21:25:39 -0500

    se crea componente de contactanos

--- src/PanelControl.jsx
+++ src/PanelControl.jsx
@@ -1,11 +1,13 @@
 import QuinesSomos from "./components/QuienesSomos";
 import Registro from "./pages/Registro";
+import Contactanos from "./components/Contactanos";
 
 function PanelControl(){
     return(
         <div className="panel-control">
             <QuinesSomos />
             <Registro />
+            <Contactanos />
             <section className="panel-control-section">
             </section>
         </div>
---
+++ src/components/Contactanos.jsx
@@ -0,0 +1,41 @@
+import './Contactanos.css';
+
+const Contactanos = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    };
+
+    return (
+        <div className="contactanos">
+            <h1>Contáctanos</h1>
+            <div className="contactanos-container">
+                <div className="contactanos-info">
+                    <div className="info-contact">
+                        <i className="fas fa-user-circle icon"></i>
+                        <h2>Información de Contacto</h2>
+                        <p>
+                            <i className="fas fa-envelope"></i>
+                            DestinoIdeal@gmail.com
+                        </p>
+                        <p>
+                            <i className="fas fa-phone"></i>
+                            604 4563788
+                        </p>
+                    </div>
+                </div>
+                <div className="contactanos-form">
+                    <h2>Envía tu Mensaje</h2>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" name="nombre" placeholder="Nombres" required />
+                        <input type="email" name="correo" placeholder="Correo Electrónico" required />
+                        <textarea name="mensaje" placeholder="Mensaje" required />
+                        <button type="submit">Enviar Mensaje</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Contactanos;
